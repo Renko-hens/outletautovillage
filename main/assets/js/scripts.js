aaa24e91f52a89220306a8e656915bfc89162461
Renko <renko.hens@yandex.ru>
2019-09-04 17:45:16 +0300

    over menu

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -255,3 +255,25 @@ Math.easeInOutQuad = function (t, b, c, d) {
 		};
 	}
 }());
+// По клику на кнопку "меню" меняется класс у навигации
+var 
+    menuButton = document.querySelector('.main-header__button');
+    menuList = document.querySelector('.main-header__navigation');
+
+    menuButton.addEventListener('click', function(event){
+        menuList.classList.toggle('main-header__navigation--show');
+    });
+
+// Скролл
+
+window.addEventListener('scroll', function(e){
+    var
+        oldScrollPosition = this.oldScroll || 0,
+        newScrollPosition = this.scrollY,
+        isScrollDown = newScrollPosition > oldScrollPosition;
+
+    document.querySelector('.main-header').classList.toggle('main-header--scroll-up', isScrollDown);
+
+    this.oldScroll = newScrollPosition;
+});
+
