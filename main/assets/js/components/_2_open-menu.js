59c2fbc5c7d41769b31b8156ec493c3395372811
Renko <renko.hens@yandex.ru>
2019-09-17 19:59:10 +0300

    added trade-in page and redemption-commercial page

--- main/assets/js/components/_2_open-menu.js
+++ main/assets/js/components/_2_open-menu.js
@@ -1,22 +1,32 @@
 // По клику на кнопку "меню" меняется класс у навигации
 var 
+    bodyElement = document.querySelector('body');
     menuButton = document.querySelector('.main-header__button');
     menuList = document.querySelector('.main-header__navigation');
 
     menuButton.addEventListener('click', function(event){
         menuList.classList.toggle('main-header__navigation--show');
+        bodyElement.classList.toggle('body__overflow--hidden');
     });
 
 // Скролл
 
 window.addEventListener('scroll', function(e){
-    var
+    if(bodyElement.classList == 'body__overflow--hidden'){
+        return false;
+    } else {
+        var
         oldScrollPosition = this.oldScroll || 0,
         newScrollPosition = this.scrollY,
         isScrollDown = newScrollPosition > oldScrollPosition;
 
+        
     document.querySelector('.main-header').classList.toggle('main-header--scroll-up', isScrollDown);
 
     this.oldScroll = newScrollPosition;
+    }
 });
+
+
+
 
