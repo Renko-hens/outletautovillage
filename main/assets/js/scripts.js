051f3628d33eda0b2a7b3336f7350e15715296c1
Renko <renko.hens@yandex.ru>
2019-11-27 18:52:43 +0300

    new page trainig-center

--- main/assets/js/scripts.js
+++ main/assets/js/scripts.js
@@ -828,3 +828,20 @@ window.addEventListener('scroll', function(e){
 
 
 
+
+// let vacancyButton = document.querySelectorAll(".vacancy__button");
+// let titleText = document.querySelectorAll(".vacancy__title");
+// let inputVacancy = document.querySelector("#inputPosition");
+
+//   if ( vacancyButton ) {
+//     for(let i = 0; i < vacancyButton.length; i++) {
+//       vacancyButton[i].addEventListener('click', function(){
+//         for(let j = 0; j < titleText.length; j++) {
+//           inputVacancy.value = titleText[j].data;
+//         };
+//       });
+//     };
+//   };
+
+  // document.getElementById('inputPosition').value= titleText[j].data;
+
